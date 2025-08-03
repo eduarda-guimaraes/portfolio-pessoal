67f6b1ffa6e16ba8692e2c386659c4631f06c45d
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2025-08-03 16:46:25 -0300

    Configuração de Deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Altere 'seu-usuario' e 'portfolio-eduarda' para os nomes corretos
 export default defineConfig({
   plugins: [react()],
+  base: '/portfolio-pessoal/',
 })
